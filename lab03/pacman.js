0f00d85360fc689a1e5321a59d68dd9d775f386d
James <jbill09@uottawa.ca>
2024-06-07 15:08:06 -0400

    finished

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -1,35 +1,123 @@
+/*  James Billinger
+    300187686
+    June 7th, 2024
+    CSI3140[Z]
+*/
+
 function createGame(n) {
-    
     let board = new Array(n).fill(".");
     let positions = new Set();
 
-    
     function getRandom() {
-        let pos;
-        do {
-            pos = Math.floor(Math.random() * n);
-        } while (positions.has(pos));
-        positions.add(pos);
-        return pos;
+        let val = Math.floor(Math.random() * n);
+
+        while (positions.has(val)) {
+            val = Math.floor(Math.random() * n);
+        }
+        positions.add(val);
+        return val;
     }
 
-    
-    let pacmanPos = getRandom();
-    board[pacmanPos] = "C";
+    let pacmanPosition = getRandom();
+    board[pacmanPosition] = "C";
 
-    let fruitPos = getRandom();
-    board[fruitPos] = "@";
-    
-    let ghostPos = getRandom();
-    board[ghostPos] = "^";
+    let fruitPosition = getRandom();
+    board[fruitPosition] = "@";
+
+    let ghostPosition = getRandom();
+    board[ghostPosition] = "^";
+
+    let gameState = {
+        board: board,
+        score: 0,
+        level: 1
+    };
+
+    console.log(gameState);
+    return gameState;
+}
 
 
+function moveLeft(gameState) {
+    let game = gameState.board;
+    const pacmanPosition = game.indexOf("C");
 
-    console.log(board);
-    return board;
+    if (pacmanPosition > 0) {
+        if (game[pacmanPosition - 1] === ".") {
+            gameState.score += 1;
+        }
+        game[pacmanPosition] = "";  
+        game[pacmanPosition - 1] = "C";  
+    }
+
+    if (game.includes(".") == false) {
+        gameState = nextLevel(gameState, game.length);
+    }
+
+    console.log(gameState);
+    return gameState;
+}
+
+function moveRight(gameState) {
+    let game = gameState.board;
+    const pacmanPosition = game.indexOf("C");
+
+    if (pacmanPosition < game.length - 1) {
+        if (game[pacmanPosition + 1] === ".") {
+            gameState.score += 1;
+        }
+        game[pacmanPosition] = "";  
+        game[pacmanPosition + 1] = "C";  
+    }
+
+    if (game.includes(".") == false) {
+        gameState = nextLevel(gameState, game.length);
+    }
+
+    console.log(gameState);
+    return gameState;
+}
+
+function nextLevel(gameState, n) {
+    let board = new Array(n).fill(".");
+    let positions = new Set();
+
+    function getRandom() {
+        let val = Math.floor(Math.random() * n);
+        
+        while (positions.has(val)) {
+            val = Math.floor(Math.random() * n);
+        }
+        positions.add(val);
+        return val;
+    }
+
+    let pacmanPosition = getRandom();
+    board[pacmanPosition] = "C";
+
+    let fruitPosition = getRandom();
+    board[fruitPosition] = "@";
+
+    let ghostPosition = getRandom();
+    board[ghostPosition] = "^";
+
+    gameState.board = board;
+    gameState.level += 1;
+
+    console.log("advance to level " + gameState.level);
+    return gameState;
 }
 
 
 
-// Test the createGame function
-createGame(12);
+
+//test
+let gameState = createGame(20);
+gameState = moveLeft(gameState);  
+gameState = moveRight(gameState); 
+gameState = moveRight(gameState); 
+gameState = moveRight(gameState); 
+gameState = moveRight(gameState); 
+gameState = moveRight(gameState); 
+
+
